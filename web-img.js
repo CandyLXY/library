d2b93152dc96f9eb26141595972c9fac8176361e
CandyLXY <72145743+CandyLXY@users.noreply.github.com>
2020-10-01 15:51:11 +0800

    Create web-img.js

---
+++ web-img.js
@@ -0,0 +1,20 @@
+//Code By CandyLXY (Chocolate Programming)
+//Images here are found online from wikipedia
+
+/* How to use:
+
+1) Code in a code editor, load the library first.
+
+2) Add a id at the img tags, only cartoon photos:
+ - Looney
+ - PinkPanther
+ - Shadow
+ - Megara
+ 
+ 3) Done!
+ */
+ 
+ document.getElementById("Looney").src = "https://upload.wikimedia.org/wikipedia/en/9/98/Cool_Cat_Looney_Tunes.png";
+ document.getElementById("PinkPanther").src = "https://upload.wikimedia.org/wikipedia/en/9/96/Pink_Panther.png";
+ document.getElementById("Shadow").src = "https://upload.wikimedia.org/wikipedia/en/thumb/4/41/ShadowTheHedgehogSA2.png/220px-ShadowTheHedgehogSA2.png";
+ document.getElementById("Megara").src = "https://upload.wikimedia.org/wikipedia/en/b/b1/Megara_promo_Disney_pose.png";
